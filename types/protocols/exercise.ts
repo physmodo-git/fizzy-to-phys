23f800619a72b022abda9064b0fd7ee38f450d17
max <marzdor@gmail.com>
2025-12-07 12:35:13 -0500

    testing

---
+++ types/protocols/exercise.ts
@@ -0,0 +1,29 @@
+export interface IProtocolExerciseModel {
+  id: string;
+  name: string;
+  thumbnail_url: string;
+  video_url: string;
+  instructions?: string;
+  library_source_id: number;
+  created_at: string;
+  updated_at: string;
+  is_active: boolean;
+  protocolExerciseEquipment?: IProtocolExerciseEquipmentModel[];
+  protocolExerciseFocusAreas?: IProtocolExerciseToFocusAreaModel[];
+  protocolExerciseLibrarySource?: IProtocolExerciseLibrarySourceModel;
+}
+
+export interface IProtocolExerciseEquipmentModel {
+  id: number;
+  name: string;
+}
+
+export interface IProtocolExerciseToFocusAreaModel {
+  exercise_id: string;
+  focus_area_id: number;
+}
+
+export interface IProtocolExerciseLibrarySourceModel {
+  id: number;
+  name: string;
+}
